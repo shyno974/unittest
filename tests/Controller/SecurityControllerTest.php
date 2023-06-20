3309ca788782b2dad11c4e0e73490cb9a2f99a29
gwendal <hubert.gwendal@gmail.com>
2023-06-20 13:14:52 +0400

    securityTest

---
+++ tests/Controller/SecurityControllerTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Test\Controller;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class UserControllerTest extends WebTestCase
+{
+    private KernelBrowser $client;
+    private UserRepository $repository;
+    private string $path = '/login';
+    public function test()
+    {
+        $this->assertEquals(42, 42);
+    }
+}
